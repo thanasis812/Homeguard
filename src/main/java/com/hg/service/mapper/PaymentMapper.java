e2540b066413641a0291ac5a5357802b3f71de90
thkos <th.kostinas@gmail.com>
2024-04-19 22:12:13 +0300

    minor changes

--- src/main/java/com/hg/service/mapper/PaymentMapper.java
+++ src/main/java/com/hg/service/mapper/PaymentMapper.java
@@ -4,6 +4,11 @@ import com.hg.domain.Payment;
 import com.hg.domain.RentalAgreement;
 import com.hg.service.dto.PaymentDTO;
 import com.hg.service.dto.RentalAgreementDTO;
+import com.hg.service.dto.mydto.UserPaymentDTO;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
 import org.mapstruct.*;
 
 /**
@@ -19,8 +24,21 @@ public interface PaymentMapper extends EntityMapper<PaymentDTO, Payment> {
     @Mapping(target = "id", source = "id")
     RentalAgreementDTO toDtoRentalAgreementId(RentalAgreement rentalAgreement);
 
-    @Named("rentalAgreementId")
-    @BeanMapping(ignoreByDefault = true)
-    @Mapping(target = "id", source = "id")
-    Payment toDtoRentalAgreementId(RentalAgreement rentalAgreement);
+    List<UserPaymentDTO> toUserPaymentDTOList(List<Payment> payment);
+
+    @Mapping(target = "houseId", source = "payment", qualifiedByName = "extractHouseId")
+    @Mapping(target = "paymentDate", source = "payedDate", qualifiedByName = "instantToLocalDate")
+    UserPaymentDTO toUserPaymentDTO(Payment payment);
+
+    @Named("extractHouseId")
+    default Long extractHouseId(Payment payment) {
+        return payment != null && payment.getRentalAgreement() != null && payment.getRentalAgreement().getProperty() != null
+            ? payment.getRentalAgreement().getProperty().getId()
+            : null;
+    }
+
+    @Named("instantToLocalDate")
+    default LocalDate instantToLocalDate(Instant instant) {
+        return instant == null ? null : instant.atZone(ZoneId.systemDefault()).toLocalDate();
+    }
 }
