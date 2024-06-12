c747007f35df7d2bf41272ea33bbc1711cb1e315
thkos <th.kostinas@gmail.com>
2024-06-12 19:45:06 +0300

    Format code deprecate some resources

--- src/main/java/com/hg/web/rest/PaymentResource.java
+++ src/main/java/com/hg/web/rest/PaymentResource.java
@@ -2,26 +2,18 @@ package com.hg.web.rest;
 
 import com.hg.repository.PaymentRepository;
 import com.hg.service.PaymentService;
-import com.hg.service.dto.PaymentDTO;
 import com.hg.service.dto.mydto.UserPaymentDTO;
-import com.hg.web.rest.errors.BaseException;
 import io.swagger.v3.oas.annotations.Hidden;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import tech.jhipster.web.util.HeaderUtil;
-import tech.jhipster.web.util.PaginationUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import tech.jhipster.web.util.ResponseUtil;
 
 /**
@@ -184,7 +176,7 @@ public class PaymentResource {
      * @param tenantId the id of the tenant to retrieve payments.
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the paymentDTO, or with status {@code 404 (Not Found)}.
      */
-    @GetMapping("userPayments/{tenantId}")
+    @GetMapping("userPayments}")
     public ResponseEntity<List<UserPaymentDTO>> getUserPaymentByTenantId(@PathVariable("tenantId") Long tenantId) {
         log.debug("REST request to get Payment : {}", tenantId);
         List<UserPaymentDTO> paymentDTO = paymentService.findUserPayments(tenantId);
